aacf9598a652fdb334b734aee6bc75db36742133
Sulaiman Sudirman <sulaiman@operatebeyond.com>
2026-03-04 18:11:12 +0800

    Removing Couchdb & Ignore data.json

--- app/plugins/pouchdb.client.ts
+++ app/plugins/pouchdb.client.ts
@@ -5,13 +5,13 @@ export default defineNuxtPlugin(() => {
   const db = new PouchDB('my_local_db')
 
   // Remote CouchDB (optional)
-  const remoteDb = new PouchDB('https://admin:password@ef0c-2001-e68-5425-1d07-edd6-679-ed5-41e3.ngrok-free.app/my_remote_db')
+  // const remoteDb = new PouchDB('https://admin:password@ef0c-2001-e68-5425-1d07-edd6-679-ed5-41e3.ngrok-free.app/my_remote_db')
 
   // Sync (optional)
-  db.sync(remoteDb, { live: true, retry: true }).on('error', function (err) {
-    // Handle errors
-    console.log(err)
-  })
+  // db.sync(remoteDb, { live: true, retry: true }).on('error', function (err) {
+  //   // Handle errors
+  //   console.log(err)
+  // })
 
   return {
     provide: {
