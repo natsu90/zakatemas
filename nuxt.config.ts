6dcf41c149d33b197baa09f87392efe9a353741f
Sulaiman Sudirman <sulaiman@operatebeyond.com>
2026-03-06 09:41:21 +0800

    Adding Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,10 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       ],
+      script: [
+        { src: 'https://www.googletagmanager.com/gtag/js?id=G-GWMVH6M68Z', async: true },
+        { innerHTML: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-GWMVH6M68Z')" },
+      ],
     },
   },
   vite: {
